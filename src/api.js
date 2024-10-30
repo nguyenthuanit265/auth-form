f504aa759bd427ebe3301c41ac063587bd39c3d3
nguyenthuanit265 <thuan.nguyenhuynhminh@sai-digital.com>
2024-10-30 22:43:42 +0700

    add log

--- src/api.js
+++ src/api.js
@@ -1,12 +1,12 @@
 // src/config/api.js
 const API_URL = process.env.REACT_APP_API_URL;
 
-// export const API_ENDPOINTS = {
-//     SIGN_IN: `${API_URL}/auth/login`,
-//     SIGN_UP: `${API_URL}/auth/sign-up`,
-// };
 export const API_ENDPOINTS = {
-    SIGN_IN: '/api/v1/auth/login',
-    SIGN_UP: '/api/v1/auth/sign-up'
+    SIGN_IN: `${API_URL}/auth/login`,
+    SIGN_UP: `${API_URL}/auth/sign-up`,
 };
+// export const API_ENDPOINTS = {
+//     SIGN_IN: '/api/v1/auth/login',
+//     SIGN_UP: '/api/v1/auth/sign-up'
+// };
 export default API_ENDPOINTS;
